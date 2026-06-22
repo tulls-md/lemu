c1d99e328116f86dbaf48ceffbad412fbbbae109
alexdem <alexdem@tulls.ru>
2026-06-22 19:09:27 +0300

    Add overview DocCards

--- documentation/src/mdx-components.tsx
+++ documentation/src/mdx-components.tsx
@@ -1,6 +1,12 @@
 import type { MDXComponents } from "mdx/types";
 import { getMDXComponents } from "@tulls-md/tulldoc";
+import { createDocContents } from "@tulls-md/tulldoc/server";
+import { join } from "path";
+
+const { DocTree, DocCards } = createDocContents(
+  join(process.cwd(), "src/content"),
+);
 
 export function useMDXComponents(): MDXComponents {
-  return getMDXComponents();
+  return getMDXComponents({ DocTree, DocCards });
 }
