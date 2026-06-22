055f3bcccfa9feefb97df694350680845ae5f393
alexdem <alexdem@tulls.ru>
2026-06-22 19:47:28 +0300

    add icon in Cell

--- ui/src/components/data/cell/cell.tsx
+++ ui/src/components/data/cell/cell.tsx
@@ -1,4 +1,5 @@
 import styles from "./cell.module.css";
+import type { Icon } from "@phosphor-icons/react";
 
 interface CellProps {
   /**
@@ -7,14 +8,21 @@ interface CellProps {
   title: string;
 
   /**
+   * Иконка, отображается слева
+   */
+  icon?: Icon;
+
+  /**
    * Описание ясчйеки ячейки. Отображается под заголовком
    */
   description?: string;
 }
 
-export function Cell({ title, description }: CellProps) {
+export function Cell({ title, description, icon: IconComponent }: CellProps) {
   return (
     <div className={styles.Root}>
+      {IconComponent && <IconComponent />}
+
       <div>
         <div className={styles.Title}>{title}</div>
         <div className={styles.Description}>{description}</div>
